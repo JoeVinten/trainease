c842527fe84e3f4e0dc9f1c7157ac296e891f6b4
Joe Vinten <joseph.vinten@gmail.com>
2025-05-27 18:47:46 +0100

    chore: inital project setup

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+	print("Welcome to train ease!\n")
+	print("Please enter your location:")
+	location = input()
+	print("Please enter your destination:")
+	destination = input()
+	print(f"You are travelling between {location} and {destination}")
+
+
+
+if __name__ == "__main__":
+	main()
